ab09bc3c84b27c985baf2b4b624bb81a82c3b39d
Devon Wasson <devonwasson@gmail.com>
2020-02-25 09:08:51 -0500

    attempt at better mobile spacing for wedding party

--- js/main.js
+++ js/main.js
@@ -149,7 +149,6 @@ AOS.init({
 
 	var carousel = function() {
 		$('.carousel-friends').owlCarousel({
-			autoplay: false,
 			autoHeight: true,
 			center: true,
 			loop: false,
@@ -161,37 +160,15 @@ AOS.init({
 			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
 			responsive:{
 				0:{
-					items: 1
+					items: 2,
+					margin: 0,
+					// stagePadding: 0
 				},
 				600:{
 					items: 2
 				},
 				1000:{
 					items: 6
-				}
-			}
-		});
-
-		$('.carousel-testimony').owlCarousel({
-			autoplay: true,
-			autoHeight: true,
-			center: true,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			dots: true,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
 				}
 			}
 		});
